78a83f01c6f43d307ec562b178610087a8ff264f
Nathan Chagar <nathan.adel.chagar@gmail.com>
2021-03-07 17:07:58 -0800

    Made MVP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Char from "./components/Character"
+import axios from "axios"
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,11 +11,26 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [people, setPeople] = useState([])
+
+  useEffect( () => {
+    axios.get('https://swapi.dev/api/people/')
+    .then((res) => {
+      setPeople(res.data)
+      // console.log(res.data, 'hi')
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }, [] )
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {people.map((elem) => {
+        return <Char key = {elem.id} people = {elem} />
+      })}
     </div>
   );
 }
-
 export default App;
